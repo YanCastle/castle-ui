f37c803816a63348ba502fb9e960dcc26ac7ce61
yanpengquan <490523604@qq.com>
2018-07-01 17:34:08 +0800

    init

---
+++ gulpfile.ts
@@ -0,0 +1,13 @@
+var pkg = require('./package.json');
+var inds = pkg.independents;
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var del = require('del');
+var gulpif = require('gulp-if');
+var minimist = require('minimist');
+var zip = require('gulp-zip');
